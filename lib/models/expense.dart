64f8b5b52e42851f6c266551176ab22f2e1e0c3a
Chiraag_Arun_Kumar <39455997+chiraag918@users.noreply.github.com>
2023-05-25 05:23:26 +0530

    Add bucket class

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 import 'package:intl/intl.dart';
@@ -31,5 +33,22 @@ class Expense {
 
   String get formattedDate {
     return dateFormatter.format(date);
+  }
+}
+
+class ExpenseBucket {
+  const ExpenseBucket({required this.category, required this.expenses});
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+
+    return sum;
   }
 }
