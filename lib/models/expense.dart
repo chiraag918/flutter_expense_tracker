518b8cc935536045d4052e988259171bc8596b9b
Chiraag_Arun_Kumar <39455997+chiraag918@users.noreply.github.com>
2023-05-25 05:34:55 +0530

    Add named constructor to filter expenses by category

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -39,6 +39,11 @@ class Expense {
 class ExpenseBucket {
   const ExpenseBucket({required this.category, required this.expenses});
 
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
   final Category category;
   final List<Expense> expenses;
 
